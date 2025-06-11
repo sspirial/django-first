912e02ad25408eb957f2cfea3cb86b00a3106780
Munubi <sspirial@gmail.com>
2025-06-11 17:05:29 +0300

    Update URL pattern to route polls app at the root path

--- mysite/urls.py
+++ mysite/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 from debug_toolbar.toolbar import debug_toolbar_urls
 
 urlpatterns = [
-    path('polls/', include('polls.urls')),
+    path('', include('polls.urls')),
     path('admin/', admin.site.urls),
 ] + debug_toolbar_urls()
