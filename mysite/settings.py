a61c185fd429e0c70268c4509c70dee7d8795470
Munubi <sspirial@gmail.com>
2025-06-11 17:28:28 +0300

    Refactor WSGI application variable and update settings for Django Debug Toolbar; add initial Vercel configuration

--- mysite/settings.py
+++ mysite/settings.py
@@ -42,8 +42,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    "debug_toolbar.middleware.DebugToolbarMiddleware",
     'django.middleware.security.SecurityMiddleware',
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
