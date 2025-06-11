c9c973295b8f5c93a723a3f4f4d1efed82c19716
Munubi <sspirial@gmail.com>
2025-06-11 16:52:35 +0300

    Update ALLOWED_HOSTS and rename WSGI application variable for deployment readiness

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-p5it0vd76=um7hx7as3*7^f)warhm!&+utn*19@w-iocqimu7k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
 
 # Application definition
@@ -69,7 +69,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'mysite.wsgi.application'
+WSGI_APPLICATION = 'mysite.wsgi.app'
 
 
 # Database
